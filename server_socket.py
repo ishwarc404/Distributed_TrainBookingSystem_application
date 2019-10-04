51335052d10d60b09859aa0fb240bd94d2b36a2d
Ishwar Choudhary <ishwar@github.com>
2019-10-04 21:28:23 +0530

    server fully connected to db for scheduling

--- server_socket.py
+++ server_socket.py
@@ -2,32 +2,59 @@ from socket import *
 import MySQLdb
 import json
 
-def database_access(packet):
+def schedule_packetparse(packet_to_parse):
+	#packet parse will basically convert the list into dictionary
+	#this will enable easy access to the db
+	packet_dictionary = {"Source":packet_to_parse[0],"Destination":packet_to_parse[1],"Date":packet_to_parse[2]}
+
+	return packet_dictionary
+
+
+def schedule_database_access(packet):
+
+	#we need to parse the packet thast we have sent from the client and 
+	# - recieved by the server socket
+
+	#lets call a packet parse function
+	parsed_packet = schedule_packetparse(packet) #packet is orignal packet
+	#parsed_packet is the pacKet after parsing the data
+
+	print("Let's print the parsed packet",parsed_packet)
+	#parsed_packet is in a dictionary format
+
+	#we will now use the information in this packet to retrive the train information
+
+
 	# Open database connection
-	db = MySQLdb.connect("localhost","root","rootroot","train_booking" )
+	#train_tkt is the name of the database
+	db = MySQLdb.connect("localhost","root","rootroot","train_tkt" ) 
 
 	# prepare a cursor object using cursor() method
 	cursor = db.cursor()
 
-	sql = "SELECT * FROM train_names"
+	#table name is trains
+	sql = 'SELECT * FROM trains where From_City= \'{}\' AND To_City=\'{}\''.format(parsed_packet["Source"],parsed_packet["Destination"])
 	cursor.execute(sql)
 
 	results = cursor.fetchall()
+	print((results))
 	for row in results:
-		print(type(row))
+		print((row))
 	
-	#print("everything came properly")
-	#now lets send this tuple bacl
-	#print(type(results)) -- tuple
-	#return row #this is just one row for now
-	return results
+	returned_row = list(results)[0]
+	returned_row = returned_row[1:] #removing the indexing and 
+	returned_row = returned_row[:-1] #removing the timing for now coz json cannot pack it
+	return returned_row
 
 
+#initialization of the data
 serverPort = 12000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 count = 0
+
+
 
 while 1:
 	count+=1
@@ -36,11 +63,18 @@ while 1:
 	packet = connectionSocket.recv(1024)
 	packet = packet.decode('utf-8')
 	#packet is coming in as a json data
-	print(packet) #this is just printing what the user is sending
+	# print(packet) #this is just printing what the user is sending
+	# print("in the server")
+	# print(type(packet))
 
 	#now lets call a function which will read the sql database and then return the values stored in it
-	values = database_access(packet)
-	return_packet = list(values[0]) #sending just the first row for now
+	packet = json.loads(packet)
+
+	#getting all the rows retrived from the database
+	values = schedule_database_access(packet)
+
+
+	return_packet = list(values) #sending just the first row for now #only 1 row will be sent back as there is only 1 train for every source and destination pair
 	print("here",return_packet)
 	return_packet = json.dumps(return_packet)
 	print("type",type(return_packet))
